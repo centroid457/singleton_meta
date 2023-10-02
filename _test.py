ad64813413a1749f37a922291f1a8c3699b357ea
admin <023>
2023-10-02 16:32:40 +0300

    add _SINGLETONS

--- _test.py
+++ _test.py
@@ -10,41 +10,42 @@ from singleton_meta import *
 
 
 # =====================================================================================================================
-def test__nesting_direct():
-    class Victim(Singleton):
+def test__nesting_direct_correct():
+    class Victim1(Singleton):
         attr = 1
 
     class Victim2(Singleton):
-        attr = 11
+        attr = 2
 
-    assert Victim().attr == 1
-    Victim().attr = 2
-    assert Victim().attr == 2
+    assert Victim1().attr == 1
+    Victim1().attr = 11
+    assert Victim1().attr == 11
 
-    assert Victim2().attr == 11
+    assert Victim2().attr == 2
     Victim2().attr = 22
     assert Victim2().attr == 22
 
-    assert Victim().attr == 2
+    assert Victim1().attr == 11
+
+    assert Singleton._SINGLETONS == [Victim1(), Victim2()]
 
 
-def test__nesting_several():
+def test__nesting_incorrect():
     class Victim(Singleton):
         attr = 1
     class Victim2(Victim):
-        attr = 11
+        attr = 2
 
     assert Victim().attr == 1
-    Victim().attr = 2
-    assert Victim().attr == 2
+    Victim().attr = 11
+    assert Victim().attr == 11
 
     # TRY USE SECOND LEVEL NESTING - ALL WRONG! dont use several levels!!!
-    assert Victim2().attr == 2  # UNEXPECTED!
+    assert Victim2().attr == 11  # UNEXPECTED!
 
     class Victim2(Victim, metaclass=SingletonMeta):
-        attr = 11
-    assert Victim2().attr == 2  # UNEXPECTED!
-
+        attr = 22
+    assert Victim2().attr == 11  # UNEXPECTED!
 
 
 # =====================================================================================================================
--- singleton_meta/main.py
+++ singleton_meta/main.py
@@ -1,17 +1,17 @@
-"""
-NOTICE: all your Singletons must be only last classes! dont use nesting from any Your Singletons!
-"""
-
+from typing import *
 from threading import Lock
 
 
+# =====================================================================================================================
 class SingletonMeta(type):
     """
     metaclass which create the singleton logic.
     USE ONLY LIKE
         class MySingleton(metaclass=_SingletonMeta):
             pass
-    but prefir use next class Singleton!
+    but prefer use next class Singleton!
+
+    :param MUTEX: mutex for safe creating items
     """
 
     MUTEX: Lock = Lock()
@@ -21,6 +21,11 @@ class SingletonMeta(type):
         if not hasattr(cls, '__INSTANCE'):
             setattr(cls, '__INSTANCE', None)
             cls.__INSTANCE = super().__call__(*args, **kwargs)
+
+            # collect from all classes
+            if Singleton in cls.__mro__:
+                Singleton._SINGLETONS.append(cls.__INSTANCE)
+
         cls.MUTEX.release()
         return cls.__INSTANCE
 
@@ -30,5 +35,22 @@ class Singleton(metaclass=SingletonMeta):
     usable class for typical nesting like
         class MySingleton(Singleton):
             pass
+
+    :param _SINGLETONS: collection of created singletons instances
+        when you create several classes, you maybe need to keep access to all of them.
+
+    USAGE
+    -----
+    BEST PRACTICE
+    Use only one level nesting!
+        class Victim1(Singleton):
+            pass
+        class Victim2(Singleton):
+            pass
+        class VictimINCORRECT(Victim1):
+            pass
     """
-    pass
+    _SINGLETONS: List['Singleton'] = []
+
+
+# =====================================================================================================================
