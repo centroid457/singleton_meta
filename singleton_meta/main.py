d4f08a0c5d4d6bf5878a1f795630fdc564359219
admin <023>
2023-10-12 22:15:31 +0300

    zero try - work step

--- singleton_meta/main.py
+++ singleton_meta/main.py
@@ -27,41 +27,51 @@ class Exx_SingletonNestingLevels(Exception):
 
 
 # =====================================================================================================================
+
+
+# =====================================================================================================================
 class SingletonMetaClass(type):
-    """
-    metaclass which create the singletons.
+    """metaclass which create the singletons.
 
     USAGE only like:
         class MySingleton(metaclass=_SingletonMeta):
             pass
     but prefer using SingletonWMetaCall!
 
-    :param _mutex_Singleton: mutex for safe creating items
+    :ivar _mutex_Singleton: mutex for safe creating items
+    :ivar _CLS_INST_BLOCKED: set of classes which can NOT be used as singleton! for correct singletons reason!
+        if we instantiate one class and then instantiate one of other class nesting this one -
+        before you did not understand your architecture mistake, but now it will rase!
+        So it prevent and raise incorrect singleton usage
+    :ivar _CLS_INST_USED: set of classes which already used as singleton!
     """
-    _mutex_Singleton: Lock = Lock()    # keep this special name! (need uniq! in case of exists in source!)
-    CLS_INST_BLOCKED: Set[Any] = set()
-    CLS_INST_USED: Set[Any] = set()
+    _mutex_Singleton: Lock = Lock()  # keep this special name! dont use just MUTEX! (need uniq! in case of exists in source!)
+    _CLS_INST_BLOCKED: Set[Any] = set()
+    _CLS_INST_USED: Set[Any] = set()
 
     def __call__(cls, *args, **kwargs):
         # check blocked --------------------------------------------
-        if cls in SingletonMetaClass.CLS_INST_BLOCKED:
+        if cls in SingletonMetaClass._CLS_INST_BLOCKED:
             msg = f"{cls.__name__=} WAS BLOCKED before by creating singleton in upper nesting level"
-            for cls_blocked in SingletonMetaClass.CLS_INST_BLOCKED:
+            for cls_blocked in SingletonMetaClass._CLS_INST_BLOCKED:
                 msg += f"\n\t{cls_blocked.__name__=}"
             raise Exx_SingletonNestingLevels(msg)
 
-        SingletonMetaClass.CLS_INST_USED.add(cls)
+        SingletonMetaClass._CLS_INST_USED.add(cls)
 
         for cls_mro in cls.__mro__[1:]:
-            if cls_mro in SingletonMetaClass.CLS_INST_USED:
+            if cls_mro in SingletonMetaClass._CLS_INST_USED:
                 msg = f"{cls.__name__=} WAS USED before by creating singleton in less nesting level"
-                for cls_used in SingletonMetaClass.CLS_INST_USED:
+                for cls_used in SingletonMetaClass._CLS_INST_USED:
                     msg += f"\n\t{cls_used.__name__=}"
                 raise Exx_SingletonNestingLevels(msg)
-            SingletonMetaClass.CLS_INST_BLOCKED.add(cls_mro)
+            SingletonMetaClass._CLS_INST_BLOCKED.add(cls_mro)
 
         # create singleton -----------------------------------------
         cls._mutex_Singleton.acquire()
+
+        cls._mro_check_blocked()
+
         if not hasattr(cls, '__INSTANCE'):
             setattr(cls, '__INSTANCE', None)
             cls.__INSTANCE = super().__call__(*args, **kwargs)
@@ -80,11 +90,15 @@ class SingletonMetaClass(type):
         cls._mutex_Singleton.release()
         return cls.__INSTANCE
 
+    @classmethod
+    def _mro_check_blocked(cls) -> Optional[NoReturn]:
+        pass
+
 
 class SingletonWMetaCall(metaclass=SingletonMetaClass):
-    """Singleton manager
+    """Singleton manager, creating them by using CALL with metaclassing
 
-    :param _SINGLETONS: collection of created singletons instances
+    :ivar _SINGLETONS: collection of created singletons instances
         when you create several classes, you maybe need to keep access to all of them.
         Used in tests!
 
@@ -114,14 +128,15 @@ class SingletonWMetaCall(metaclass=SingletonMetaClass):
 
 
 class SingletonWoMetaNew:
-    """else one variant after SingletonWMetaCall.
+    """Singleton manager, creating them by using NEW without metaclassing
+    else one variant after SingletonWMetaCall, in case of metaclass is not acceptable.
 
     USEFUL CASES:
     1. you need to use some metaclass (cant set two metaclasses).
     2. always reinit instance on instantiating! (would be called __init__(*args/kwargs)!
         if blank init - will be the same as SingletonWMetaCall
 
-    params see in SingletonWMetaCall
+    attributes see in SingletonWMetaCall
     """
     _SINGLETONS: List['SingletonWoMetaNew'] = []
     _mutex_Singleton: Lock = Lock()
