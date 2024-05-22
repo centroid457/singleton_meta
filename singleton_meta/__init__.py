259722e2201a69ee81dd68a8720f810767b2b553
Andrei Starichenko <centroid@mail.ru>
2024-05-22 16:47:47 +0300

    [__INIT__.py] fix import+apply last pypi template

--- singleton_meta/__init__.py
+++ singleton_meta/__init__.py
@@ -1,1 +1,35 @@
-from .main import *
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .main import (
+    # BASE
+    SingletonManagerBase,
+    SingletonMetaCallClass,
+    SingletonByCallMeta,
+    SingletonByNew,
+
+    # AUX
+
+    # TYPES
+
+    # EXX
+    Exx_SingletonNestingLevels,
+)
+
+# =====================================================================================================================
